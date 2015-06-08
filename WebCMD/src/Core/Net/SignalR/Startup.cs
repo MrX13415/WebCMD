efaf292d521bf8088668bfe1c936037a7f21cb7a
MrX13415 <mr.x.13415@googlemail.com>
2015-06-08 20:37:35 +0200

    Commit of missing files

---
+++ WebCMD/src/Core/Net/SignalR/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+[assembly: OwinStartup(typeof(WebCMD.Core.Net.SignalR.Startup))]
+
+namespace WebCMD.Core.Net.SignalR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+
+            HubConfiguration config = new HubConfiguration();
+            app.MapSignalR("/sys/script/signalr", config);
+        }
+    }
+}
